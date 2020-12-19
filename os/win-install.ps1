4fc58b2feb238cfa9f7d9f9688131f7fe64d6835
Aneel Rathore <aneelatwork@gmail.com>
2020-12-19 21:04:45 +1100

    install conan from pip

--- os/win-install.ps1
+++ os/win-install.ps1
@@ -21,4 +21,5 @@
 #SOFTWARE.
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-chocolatey install git cmake llvm pyton pip sed conan neovim nodejs ninja keepass -y
+chocolatey install git cmake llvm pyton pip sed neovim nodejs ninja keepass -y
+pip install conan cpplint
